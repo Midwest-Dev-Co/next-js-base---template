66b6000511024aea4aa25f1a048be3ec261241ec
WilderDev <williamjohnwilder@outlook.com>
2024-05-21 21:33:39 -0500

    Versioning untracked files

--- src/components/ui/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import Section from '@/components/containers/Section';
-
-export default function Footer() {
-  return (
-    <Section name="Footer">
-      <h1 className="text-center">Footer</h1>
-    </Section>
-  );
-}
